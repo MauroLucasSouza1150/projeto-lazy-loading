a687c0ebc171dc1d86585c6c6e59ef93aebabacd
Mauro Lucas <maurolucas2014@gmail.com>
2023-09-07 17:02:35 -0300

    comitando projeto

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const imagens = document.querySelectorAll(".imagem-container img");
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+
+        const imagem = entry.target;
+
+        imagem.src = imagem.src.replace("&w=10&", "&w=1000&");
+
+        observer.unobserve(imagem);
+    });
+}, {});
+
+imagens.forEach((imagem) => {
+    observer.observe(imagem);
+});
